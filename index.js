a0266bd1b13de6fc9bff322ba0d6714c2987adea
Mike Galakos <mgalakos@gmail.com>
2017-04-01 01:24:46 +0300

    devo -> webo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const isDev = process.env.NODE_ENV !== 'production'
 if (isDev) {
   module.exports = require('./lib/server/middleware')
 } else {
-  module.exports = config => (req, res, next) => next() // nullify devo on production
+  module.exports = config => (req, res, next) => next() // nullify webo on production
 }
