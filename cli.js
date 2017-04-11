2a5040ba1e13c6e3dc8587fd27d4d57dea2bedcb
mike work <mgalakos@eoppep.gr>
2017-04-11 16:40:42 +0300

    refactor config

--- cli.js
+++ cli.js
@@ -1,26 +1,19 @@
 #! /usr/bin/env node
 
 const [ , , projectType, root ] = process.argv
-global.COLORS = { GREEN: '\x1b[32m', RED: '\x1b[31m', YELLOW: '\x1b[33m', DIM: '\x1b[2m', RESET: '\x1b[0m' }
 
-
-const path = require('path')
+const { log } = require('./lib/utils')
 
 if (!projectType || !root) { console.log(`Usage : webo express [express_server_path]`); process.exit() }
-console.log(`${COLORS.GREEN}webo starting${COLORS.RESET}`)
 
-
-let config = {}
-try {
-  config = require(path.resolve('./webo.config.js'))
-} catch (error) { }
-
+require('./lib/config')
+log('GREEN', 'webo starting')
 
 switch (projectType) {
   case 'express':
-    require('./lib/express')(config, root)
+    require('./lib/express')(root)
     break;
   default:
-    console.log(`${COLORS.RED}Not recognized project type ${projectType} ${COLORS.RESET}`)
+    log('RED', `Not recognized project type ${projectType}`)
     process.exit()
 }
