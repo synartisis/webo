a5ca7089787d98d96dab19a96bfa62c2e46e8260
Mike Galakos <mgalakos@gmail.com>
2017-04-19 17:39:23 +0300

    verbose flag

--- cli.js
+++ cli.js
@@ -1,13 +1,16 @@
 #! /usr/bin/env node
 
 const [ , , projectType, root ] = process.argv
+const cliFlags = process.argv.filter(o => o.startsWith('-')).map(o => o.replace(/^-+/g, ''))
 
 const { log } = require('./lib/utils')
 
 if (!projectType || !root) { log(`Usage : webo express [express_server_path]`); process.exit() }
 
-require('./lib/config')
+const config = require('./lib/config')
+config.cliFlags = cliFlags
 log('GREEN', 'webo starting')
+log('TIMESTAMP')
 
 switch (projectType) {
   case 'express':
