bb572fa2198a2456ed5d555d8e902054e6e6bf39
Mike Galakos <mgalakos@gmail.com>
2017-07-02 21:31:50 +0300

    port to v1 #1

---
+++ cli.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+const state = require('./lib/state')
+
+let [ , ,command, root ] = process.argv
+if (!command) command = 'dev'
+if (!root) root = '.'
+
+;(async () => {
+
+  const started = new Date()
+  console.log('==', started.toISOString().split('T')[1], '==')
+
+  state.config.srcRoot = root
+  // try {
+  //   if (root !== '.') process.chdir(root)
+  // } catch (error) {
+  //   console.error(error.code === 'ENOENT' ? `Directory ${root} not found` : error)
+  //   process.exit(1)
+  // }
+  await state.init()
+
+  switch (command) {
+    case 'dev':
+      await require('./lib/dev/dev')
+      break;
+
+    case 'build':
+      await require('./lib/build/build')
+      break;
+
+    default:
+      console.log(`command ${command} does not exist`)
+      break;
+  }
+
+  const ended = new Date()
+  console.log('==', ended.toISOString().split('T')[1], '==')
+  console.log('== Time elapsed', (ended.getTime() - started.getTime()) / 1000, 's ==')
+
+})()
