559adeb98d071d7290d41ee741dfebdedca11b58
mike work <mgalakos@eoppep.gr>
2017-07-05 19:16:28 +0300

    port to v1 #5

--- cli.js
+++ cli.js
@@ -1,43 +1,28 @@
 #! /usr/bin/env node
 
-const state = require('./lib/state')
+const { init } = require('./lib/state')
+const { checkVersion } = require('./lib/utils')
+
+checkVersion()
 
 let [ , ,command, root ] = process.argv
 if (!command) command = 'dev'
-if (!root) root = '.'
+
 
 ;(async () => {
 
   const started = new Date()
   console.log('==', started.toISOString().split('T')[1], '==')
 
-  state.config.srcRoot = root
-  // try {
-  //   if (root !== '.') process.chdir(root)
-  // } catch (error) {
-  //   console.error(error.code === 'ENOENT' ? `Directory ${root} not found` : error)
-  //   process.exit(1)
-  // }
 
-  switch (command) {
-    case 'dev':
-      await state.init('dev')
-      await require('./lib/dev/dev')()
-      break;
+  if (['dev', 'build'].includes(command)) await init(command, root)
 
-    case 'build':
-      await state.init('build')
-      await require('./lib/build/build')()
-      break;
-
-    case 'init':
-      await require('./lib/init/init')()
-      break;
-
-    default:
-      console.log(`command ${command} does not exist`)
-      break;
+  if (['dev', 'build', 'init'].includes(command)) {
+    await require(`./lib/${command}/${command}`)()
+  } else {
+    console.error(`command ${command} does not exist`)
   }
+    
 
   const ended = new Date()
   console.log('==', ended.toISOString().split('T')[1], '==')
