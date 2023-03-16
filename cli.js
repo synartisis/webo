0e330a9bbebe38ec416b5930e9af5e4ffa3c00c0
mike galakos <mgalakos@gmail.com>
2023-03-16 13:34:32 +0200

    better error coloring

--- cli.js
+++ cli.js
@@ -16,5 +16,5 @@ webo(config, nodeArgs)
 })
 
 
-process.on('unhandledRejection', r => { console.error(r); process.exit(1) })
+process.on('unhandledRejection', r => { config.verbose ? logv(`\r_LIGHTRED_${r.stack ?? r}`) : log(`\r_LIGHTRED_${r}`); process.exit(1) })
 process.on('SIGINT', () => { log('\r_GREEN_webo ended by user'); process.exit(1) })
--- parsers/utils/cachebuster.js
+++ parsers/utils/cachebuster.js
@@ -5,11 +5,17 @@ const { calcFilenameHash, calcContentHash } = require('../../lib/utils.js')
 const { parsable } = require('../../webo-settings.js')
 
 exports.cachebust = async function cachebust(filename, config, { type, referrer } = {}) {
-  if (!parsable(filename)) return calcFilenameHash(filename)
+  if (!parsable(filename)) {
+    try {
+      return await calcFilenameHash(filename)
+    } catch (error) {
+      throw new Error(`cannot find ${path.relative('.', filename)} referred by ${path.relative('.', referrer)}`, { error })
+    }
+  }
   const file = getFile(filename)
   if (file.hash) return file.hash
   const { content } = await parse(filename, config, { type, cacheContent: config.command === 'build' })
-  if (!content) throw Error(`cannot found ${path.relative('.', filename)} referred by ${path.relative('.', referrer)}`)
+  if (!content) throw new Error(`cannot find ${path.relative('.', filename)} referred by ${path.relative('.', referrer)}`)
   const hash = await calcContentHash(content)
   return hash
 }
