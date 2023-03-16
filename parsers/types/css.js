31ffc674ab626c022fe8bfa61c67ef9b07b3d36e
mike galakos <mgalakos@eoppep.gr>
2023-03-16 11:29:50 +0200

    css cacheBusting

--- parsers/types/css.js
+++ parsers/types/css.js
@@ -1,5 +1,35 @@
+const path = require('path')
+const { cachebust } = require('../utils/cachebuster.js')
+
 exports.parse = async function parse(source, filename, config) {
 
-  return { content: source }
+  let content = source
 
+  if (config.cachebust) {
+    content = await cacheBusting(content, filename, config)
+  }
+
+  return { content }
+
+}
+
+
+const reStripComments = /(?!<\")\/\*[^\*]+\*\/(?!\")/
+const reRefs = /url\((?<ref>.+)\)/g
+/** @type (content: string, filename: string, config: object) => Promise<string> */
+async function cacheBusting(content, filename, config) {
+  const dir = path.dirname(filename)
+  const contentWithoutComments = content.replace(reStripComments)
+
+  const matches = contentWithoutComments.matchAll(reRefs)
+  for await (const match of matches) {
+    const ref = match.groups.ref.trim()
+    if (!ref) continue
+    const hash = await cachebust(path.join(dir, ref), config)
+    const ext = ref.split('.').pop()
+    const refFinal = ref.substring(0, ref.length - ext.length) + hash + '.' + ext
+    content = content.replaceAll(ref, refFinal)
+  }
+
+  return content
 }
